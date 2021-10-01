e5407a887164320ff7df2dc92606bdf77282ee2d
pupil <pupil@ivkhk.ee>
2021-10-01 09:09:46 +0300

    name

--- src/JKTV20Task5ArtjomKivonen/JKTV20Task5ArtjomKivonen.java
+++ src/JKTV20Task5ArtjomKivonen/JKTV20Task5ArtjomKivonen.java
@@ -35,7 +35,28 @@ public class JKTV20Task5ArtjomKivonen {
                 jaggedArray[i][j] = random.nextInt(100-50);
             }
         }
-        System.out.println(Array.toString(jaggedArray[i][j]));
+        
+        for (int i=0; i<jaggedArray.length; i++) {
+            for (int j = 0; j<jaggedArray[i].length; j++) {
+                for(int k=0; k<jaggedArray.length; k++){
+                    for(int m=0; m<jaggedArray[k].length; m++){
+                        if(jaggedArray[i][j]<jaggedArray[k][m]){
+                            jaggedArray[i][j]=jaggedArray[i][j]+jaggedArray[k][m];
+                            jaggedArray[k][m]=jaggedArray[i][j]-jaggedArray[k][m];
+                            jaggedArray[i][j]=jaggedArray[i][j]-jaggedArray[k][m];
+                        }
+                        
+                    }
+                }
+            }
+        }
+        for (int i=0; i<jaggedArray.length; i++) {
+            for (int j = 0; j<jaggedArray[i].length; j++) {
+                System.out.print(jaggedArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
     }
 
 
